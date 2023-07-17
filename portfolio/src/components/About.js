3ee6f743fea4e4329ccb269d425303eba9e9b0dd
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-07-17 17:34:07 +0200

    style: picture

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,17 +1,41 @@
 import { useContext, useState } from "react";
 import styled from "styled-components";
+import myself from "../assets/myself.png";
 import { GlobalContext } from "../context/GlobalContext"
 
 
 const Container = styled.section`
   gap: 20px;
-  div{
+  >div{
     display: flex;
     justify-content: end;
     gap: 16px;
-    
+  }
+
+  span{
+    font-size: 16px;
+    font-weight: 200;
+    line-height: 24px;
+    text-align: justify;
   }
 `
+
+const PhotoBox = styled.span`
+  figure{
+    float: left;
+    width: 94px;
+    height: 135px;
+    border-radius: 12px;
+    margin-right: 16px;
+    margin-top: 4px;
+    img{
+      object-fit: cover;
+      height: 120%;
+      width: 100%;
+    }
+  }
+`
+
 export const About = () => {
   const [textSize, setTextSize] = useState(0)
   const { language } = useContext(GlobalContext)
@@ -19,14 +43,25 @@ export const About = () => {
   return(
     <Container id="about">
       <div>
-        <span onClick={() => setTextSize(0)} className="smallBtn">{language === '/pt'? 'Curto' : 'Short'}</span>
-        <span onClick={() => setTextSize(1)} className="smallBtn">{language === '/pt'? 'Longo' : 'Long'}</span>
+        <button onClick={() => setTextSize(0)} className="smallBtn">{language === '/pt'? 'Curto' : 'Short'}</button>
+        <button onClick={() => setTextSize(1)} className="smallBtn">{language === '/pt'? 'Longo' : 'Long'}</button>
       </div>
         {language === '/pt' ?
-          (textSize === 0 ? <p>Desenvolvedora Web Full Stack formada pelo bootcamp de Programação da Labenu, onde desenvolvi habilidades técnicas de frontend e backend. Entre meus conhecimentos, posso destacar JavaScript, TypeScript, React, NodeJS, SQL e Jest. Graduada em Engenharia Civil, vivenciei experiências acadêmicas e profissionais em canteiro de obra e escritório de projetos, o que aprimorou habilidades como a comunicação eficiente, gestão de tempo, criticidade e trabalho em equipe.</p>
+          (textSize === 0 ? 
+          <PhotoBox>
+            <figure>
+              <img src={myself}/>
+            </figure>
+            <span>Desenvolvedora Web Full Stack formada pelo bootcamp de Programação da Labenu, onde desenvolvi habilidades técnicas de frontend e backend. Entre meus conhecimentos, posso destacar JavaScript, TypeScript, React, NodeJS, SQL e Jest. Graduada em Engenharia Civil, vivenciei experiências acadêmicas e profissionais em canteiro de obra e escritório de projetos, o que aprimorou habilidades como a comunicação eficiente, gestão de tempo, criticidade e trabalho em equipe.</span>
+          </PhotoBox>
           :
           <>
-          <p>Sou Desenvolvedora Web Full-Stack formada pelo bootcamp de Programação Web Full Stack Integral da Labenu, onde desenvolvi habilidades técnicas de frontend e backend. Entre elas estão HTML, CSS, JavaScript, React, Styled-Components, React Hooks, REST, Api Restful, HTTP, Jest, Node.JS, Knex, TypeScript, MySQL, AWS, SQL, Git, Github, Testes unitários e UI Design, além de mais de 1000 horas de experiência prática, guiadas por metodologias ágeis (Kanban/Scrum).</p>
+          <PhotoBox>
+            <figure>
+              <img src={myself}/>
+            </figure>
+            <span>Sou Desenvolvedora Web Full-Stack formada pelo bootcamp de Programação Web Full Stack Integral da Labenu, onde desenvolvi habilidades técnicas de frontend e backend. Entre elas estão HTML, CSS, JavaScript, React, Styled-Components, React Hooks, REST, Api Restful, HTTP, Jest, Node.JS, Knex, TypeScript, MySQL, AWS, SQL, Git, Github, Testes unitários e UI Design, além de mais de 1000 horas de experiência prática, guiadas por metodologias ágeis (Kanban/Scrum).</span>
+          </PhotoBox>
           
           <p>Sou graduada em Engenharia Civil, área de estudo que me identifiquei durante grande parte da minha vida. Isso porque tenho grande interesse pelas ciências exatas, pela solução de problemas e pelo processo de construção e materialização de ideais. Durante e após a graduação, tive várias oportunidades de aprender mais sobre o trabalho em equipe, a comunicação eficiente, o pensamento crítico e criativo, além de, constantemente, desenvolver a inteligência emocional e a resiliência. </p>
           
@@ -36,10 +71,20 @@ export const About = () => {
 
         :
           (textSize === 0 ? 
-          <p>Full Stack Web Developer graduated from Labenu's Programming bootcamp, where I developed technical skills in both frontend and backend. Among my knowledge, I can highlight JavaScript, TypeScript, React, NodeJS, SQL, and Jest. With a degree in Civil Engineering, I have experienced academic and professional opportunities in construction sites and project offices, which enhanced skills such as effective communication, time management, critical thinking, and teamwork.</p>
+          <PhotoBox>
+            <figure>
+              <img src={myself}/>
+            </figure>
+            <span>Full Stack Web Developer graduated from Labenu's Programming bootcamp, where I developed technical skills in both frontend and backend. Among my knowledge, I can highlight JavaScript, TypeScript, React, NodeJS, SQL, and Jest. With a degree in Civil Engineering, I have experienced academic and professional opportunities in construction sites and project offices, which enhanced skills such as effective communication, time management, critical thinking, and teamwork.</span>
+          </PhotoBox>
           :
           <>
-          <p>I am a Full-Stack Web Developer, having completed Labenu's Full-Stack Web Development bootcamp. Throughout the program, I honed my skills in both frontend and backend development. I am well-versed in technologies such as HTML, CSS, JavaScript, React, Styled-Components, React Hooks, REST, RESTful APIs, HTTP, Jest, Node.js, Knex, TypeScript, MySQL, AWS, SQL, Git, GitHub, Unit Testing, and UI Design. With over 1000 hours of practical experience under my belt, guided by agile methodologies like Kanban/Scrum, I am confident in my abilities.</p>
+          <PhotoBox>
+            <figure>
+              <img src={myself}/>
+            </figure>
+            <span>I am a Full-Stack Web Developer, having completed Labenu's Full-Stack Web Development bootcamp. Throughout the program, I honed my skills in both frontend and backend development. I am well-versed in technologies such as HTML, CSS, JavaScript, React, Styled-Components, React Hooks, REST, RESTful APIs, HTTP, Jest, Node.js, Knex, TypeScript, MySQL, AWS, SQL, Git, GitHub, Unit Testing, and UI Design. With over 1000 hours of practical experience under my belt, guided by agile methodologies like Kanban/Scrum, I am confident in my abilities.</span>
+          </PhotoBox>
 
           <p>Before pursuing a career in web development, I graduated in Civil Engineering, a field that fascinated me for many years. This background instilled in me a passion for exact sciences, problem-solving, and the process of turning ideas into reality through construction. Throughout my academic and professional journey, I honed essential skills such as effective communication, critical and creative thinking, teamwork, emotional intelligence, and resilience.</p>
 
