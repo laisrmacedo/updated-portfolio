cf38549cb4f29914cc939ebd2e74decde7054c79
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-08-25 10:10:27 +0200

    refactor: copyright

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -25,14 +25,15 @@ export const Footer = () => {
         <>
           <p>Codificado em Visual Studio Code por mim mesma. Construído com React e implantado com Vercel. Todo o texto é definido no tipo de letra <a href="https://fonts.google.com/specimen/Josefin+Sans?query=jose">Josefin Sans</a></p>
           <p>Imagem de cabeçalho animada criada por Myriade - <a href="https://lottiefiles.com/animations/waves-in-gradient-1-gKtmMPeAUx">LottieFiles</a></p>
+          <p>© 2023 - Todos os direitos reservados.</p>
         </>
         :
         <>
           <p>Coded in Visual Studio Code by yours truly. Built with React and deployed with Vercel. All text is set in font <a href="https://fonts.google.com/specimen/Josefin+Sans?query=jose">Josefin Sans</a></p>
           <p>Animated header image created by Myriade - <a href="https://lottiefiles.com/animations/waves-in-gradient-1-gKtmMPeAUx">LottieFiles</a></p>
+          <p>© 2023 - All rights reserved.</p>
         </>
       }
-      <p>© Copyright 2023</p>
     </Container>
   )
 }
